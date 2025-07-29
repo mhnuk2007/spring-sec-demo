4f5b955e88bab3117e916a6e647f1cd11b96e242
Mohan Lal <mhnuk2007@gmail.com>
2025-07-29 08:54:25 +0500

    Analyze and review codebase: status, add, commit, push

--- src/main/java/com/learning/springsecdemo/HelloController.java
+++ src/main/java/com/learning/springsecdemo/HelloController.java
@@ -1,19 +1,19 @@
 package com.learning.springsecdemo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String greet(){
-        return "Hello World";
+    public String greet(HttpServletRequest request){
+        return "Hello World" + request.getSession().getId();
     }
 
     @GetMapping("/about")
-    public String about(){
-        return "Telusko";
+    public String about(HttpServletRequest request){
+        return "Telusko" + request.getSession().getId();
     }
 
 }
---
+++ src/main/java/com/learning/springsecdemo/StudentController.java
@@ -0,0 +1,7 @@
+package com.learning.springsecdemo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+}
