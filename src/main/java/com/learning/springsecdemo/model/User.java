ef3e0dbce24ff1557070158dc2469d131aee3655
Mohan Lal <mhnuk2007@gmail.com>
2025-08-03 22:02:30 +0500

    Refactor: update  security configuration and enable token generation

--- src/main/java/com/learning/springsecdemo/model/User.java
+++ src/main/java/com/learning/springsecdemo/model/User.java
@@ -1,16 +1,16 @@
 package com.learning.springsecdemo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Data
 @Entity
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private int id;
     private String username;
     private String password;
--- src/main/java/com/learning/springsecdemo/service/JwtService.java
+++ src/main/java/com/learning/springsecdemo/service/JwtService.java
@@ -1,10 +1,56 @@
 package com.learning.springsecdemo.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
+
+    private String secretKey;
+
+    public JwtService() {
+        secretKey = generateSecretKey();
+    }
+
+
+    public String generateSecretKey() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGen.generateKey();
+            System.out.println("Secrete Key: " + secretKey.toString());
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Failed to generate secret key", e);
+        }
+    }
+
     public String generateToken(String username) {
 
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 3))
+                .signWith(getKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
